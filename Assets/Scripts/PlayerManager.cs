0b669aed1d2f8e54937ef69aabbb94936569a7bc
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2017-03-04 11:25:33 +0100

    [03] Add walking and running scripts - change state on key hold

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		HandleWalking ();
+		HandleRunning ();
+	}
+
+	private void HandleWalking() {
+		HandleKeyHold (KeyCode.W, "State", 1, 0);
+	}
+
+	private void HandleRunning() {
+		HandleKeyHold (KeyCode.R, "State", 2, 0);
+	}
+
+	private void HandleKeyHold(KeyCode keyCode, string parameter, int downState, int upState) {
+		if (Input.GetKeyDown (keyCode)) {
+			animator.SetInteger (parameter, downState);
+		}
+
+		if (Input.GetKeyUp (keyCode)) {
+			animator.SetInteger (parameter, upState);
+		}
+	}
+}
