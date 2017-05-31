e36a5beb9d2ec7e69b686fc074d3e7477f37847f
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2017-05-31 19:48:02 +0200

    Move files upwards

---
+++ Assets/Scripts/CharacterScripts/EnemyManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyManager : BaseCharacterManager
+{
+    public new void Start()
+    {
+        base.Start();
+        Health = 2;
+    }
+
+    public new void Update()
+    {
+        Wander();
+        base.Update();
+    }
+
+    private void Wander()
+    {
+
+    }
+}
